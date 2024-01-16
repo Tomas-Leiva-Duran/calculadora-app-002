c183ba38693206739ca248603295f685255a40be
Tomás Durán <101905549+Tomas-Leiva-Duran@users.noreply.github.com>
2024-01-16 19:55:28 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'calculadora.app.v002',
+  appName: 'calculadora-app-002',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
